50c873f1856aeb380b4f5041bec48252ea38219a
cdang730 <doradang730@gmail.com>
2026-03-28 19:46:26 -0400

    genre

--- backend/services/genre.py
+++ backend/services/genre.py
@@ -1,7 +1,8 @@
-
-
-def extract_genres_from_artists(artists: list[dict]) -> list[str]:
+def extract_genres_from_artists(artists):
     genres = []
     for artist in artists:
-        genres.extend(artist["genres"])
-    return list(set(genres))
+        # use .get and ensure it's a list
+        artist_genres = artist.get("genres") or []
+        if isinstance(artist_genres, list):
+            genres.extend(artist_genres)
+    return genres
