7c5d640bf3ba09314dca5eac32569b1f6e5aee76
cdang730 <doradang730@gmail.com>
2026-03-28 14:41:35 -0400

    change services playlist

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"], # change if needed
+    allow_origins=["*"], # change if needed
     allow_credentials=False, 
     allow_methods=["*"], 
     allow_headers=["*"],
@@ -20,3 +20,10 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "q": q}
+
+
+
+# cd backend
+# pip install python-dotenv
+# poetry install
+# pip install httpx
